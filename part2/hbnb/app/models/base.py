327db8befd8a6c51701bb4ab6540fe1c151e20cd
ChaffyLace <132900529+ChaffyLace@users.noreply.github.com>
2026-03-13 23:07:56 +0100

    Refactor BaseModel to use SQLAlchemy for ORM

--- part2/hbnb/app/models/base.py
+++ part2/hbnb/app/models/base.py
@@ -1,15 +1,18 @@
+from app import db
 import uuid
 from datetime import datetime
 
-class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+class BaseModel(db.Model):
+    __abstract__ = True 
+
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
     def save(self):
         """Update the updated_at timestamp whenever the object is modified"""
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
+        db.session.commit()
 
     def update(self, data):
         """Update the attributes of the object based on the provided dictionary"""
