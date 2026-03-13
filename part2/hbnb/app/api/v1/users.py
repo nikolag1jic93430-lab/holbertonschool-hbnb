8f44a308875946549f86c85cc359dfc4ea931eb4
ChaffyLace <132900529+ChaffyLace@users.noreply.github.com>
2026-03-13 23:45:50 +0100

    Fix syntax errors in user API implementation

--- part2/hbnb/app/api/v1/users.py
+++ part2/hbnb/app/api/v1/users.py
@@ -6,7 +6,7 @@ api = Namespace('users', description='User operations')
 user_model = api.model('User', {
     'first_name': fields.String(required=True, description='First name of the user'),
     'last_name': fields.String(required=True, description='Last name of the user'),
-    'email': fields.String(required=True, description='Email of the user')
+    'email': fields.String(required=True, description='Email of the user'),
     'password': fields.String(required=True, description='Password of the user')
 })
 
