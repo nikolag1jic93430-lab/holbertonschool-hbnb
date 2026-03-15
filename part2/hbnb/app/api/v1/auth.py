5f812bd5bdd45b44d33b4ca10a82544f4474a2af
ChaffyLace <132900529+ChaffyLace@users.noreply.github.com>
2026-03-15 17:19:11 +0100

    Update auth.py

--- part2/hbnb/app/api/v1/auth.py
+++ part2/hbnb/app/api/v1/auth.py
@@ -1,1 +1,26 @@
+from flask_restx import Namespace, Resource, fields
+from flask_jwt_extended import create_access_token
+from app.services import facade
+from flask_bcrypt import check_password_hash
 
+api = Namespace('auth', description='Opérations d\'authentification')
+
+login_model = api.model('Login', {
+    'email': fields.String(required=True, description='Email de l\'utilisateur'),
+    'password': fields.String(required=True, description='Mot de passe de l\'utilisateur')
+})
+
+@api.route('/login')
+class LoginResource(Resource):
+    @api.expect(login_model)
+    def post(self):
+        """Authentifie un utilisateur et retourne un token JWT"""
+        data = api.payload
+        user = facade.get_user_by_email(data['email'])
+
+        if user and check_password_hash(user.password, data['password']):
+            # On stocke l'email et le statut admin dans le token
+            access_token = create_access_token(identity={'email': user.email, 'is_admin': user.is_admin})
+            return {'access_token': access_token}, 200
+        
+        return {'msg': 'Email ou mot de passe invalide'}, 401
