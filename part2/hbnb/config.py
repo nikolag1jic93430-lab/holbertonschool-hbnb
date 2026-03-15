ad92dd340d6a65410c8aa0e6e42bf3ef17523302
ChaffyLace <132900529+ChaffyLace@users.noreply.github.com>
2026-03-15 18:17:12 +0100

    Update config.py

--- part2/hbnb/config.py
+++ part2/hbnb/config.py
@@ -1,13 +1,16 @@
 import os
 
+# On récupère le chemin du dossier où se trouve ce fichier config.py
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'votre_secret_jwt')
     DEBUG = False
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    # URI de la base de données (ici on utilise SQLite pour le dev)
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///development.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'hbnb_test.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 config = {
