23e25ee4bf8e4fbca9575516880c97dfff9380c4
ChaffyLace <132900529+ChaffyLace@users.noreply.github.com>
2026-03-30 22:39:25 +0200

    Refactor app initialization in __init__.py

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -4,12 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 
-# Initialisation des extensions
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 jwt = JWTManager()
 
-# Configuration de la sécurité pour Swagger
 authorizations = {
     'Bearer Auth': {
         'type': 'apiKey',
@@ -23,12 +21,10 @@ def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-    # Initialisation des applications avec l'instance Flask
     db.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
 
-    # Configuration de l'API avec les autorisations
     api = Api(
         app,
         version='1.0',
@@ -39,14 +35,12 @@ def create_app(config_class="config.DevelopmentConfig"):
         security='Bearer Auth'
     )
 
-    # Importation des namespaces
     from app.api.v1.users import api as users_ns
     from app.api.v1.amenities import api as amenities_ns
     from app.api.v1.places import api as places_ns
     from app.api.v1.reviews import api as reviews_ns
-    from app.api.v1.auth import api as auth_ns  # Ajout de l'authentification
+    from app.api.v1.auth import api as auth_ns
 
-    # Ajout des namespaces à l'API
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
